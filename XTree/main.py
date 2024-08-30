fcc33dbc6bc0898630dfb57a65cd4ca5290109f1
Palash <31160400+palashbadjatya01@users.noreply.github.com>
2024-08-30 17:01:11 -0400

    scraper changes

--- XTree/main.py
+++ XTree/main.py
@@ -1,8 +1,11 @@
 from scraper.scraperJob import ScraperJob
 
 if __name__ == '__main__':
-    website_url = ['https://www.merriam-webster.com/dictionary/article'
-                   , 'https://www.merriam-webster.com/dictionary/learn'
-                ]
-    for url in website_url:
+    website_urls = [
+        'https://www.merriam-webster.com/dictionary/article',
+        'https://www.merriam-webster.com/dictionary/learn'
+    ]
+    
+    for url in website_urls:
         scrape_data = ScraperJob(url)
+        scrape_data.determine_scraper()
--- XTree/scraper/scraperJob.py
+++ XTree/scraper/scraperJob.py
@@ -1,15 +1,23 @@
-import tldextract
-from template.template1 import word_template
+# from template.template1 import word_template
+import cfscrape
+from lxml import html    
 
 class ScraperJob:
     def __init__(self, website_url):
         self.url = website_url
-        extract = tldextract.extract(self.url)
-        self.domain = extract.domain
-        print(f"Hi from ScraperJob, {self.website_url}")
+        self.domain = website_url.split('/')[2]
+        print(f"Hi from ScraperJob, {self.url}")
 
     def determine_scraper(self):
         print(self.domain)
         if self.domain == "www.merriam-webster.com":
-            print(self.domain)
+            print(f"Scraping data from {self.domain}")
+            scraper = cfscrape.create_scraper()  
+            response = scraper.get(self.url).text 
+            return html.fromstring(response)
+            
+            xpath = '//div[@class="pd-title d-flex flex-column align-items-center align-items-md-start pb-2 pb-md-0"]'
+            elements = scrape_data.xpath(xpath)
 
+            for id, element in enumerate(elements, start=1):
+                word = element.xpath()
